e9395b5e5373130a1d1de197cf780e6bc55e4dfc
Adeola Quadri <adeolaakorede27@gmail.com>
2024-09-26 11:25:44 +0100

    changes

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ app.set('view engine','ejs')
 app.get('/', (req, res)=>{
     res.render('home')
 })
-
+app.get('/login', (req, res)=>{
+    res.render('login')
+})
 app.listen(port, ()=>console.log(`server is listening to ${port}`))
